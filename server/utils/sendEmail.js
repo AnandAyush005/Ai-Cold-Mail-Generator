6ad966168d4a5a5b12cabede38c35413357d55b0
anandayush005 <anandayush233@gmail.com>
2026-06-25 13:58:56 +0530

    Update email transport configuration to use SMTP host and port

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -6,7 +6,8 @@ async function sendEmail(options) {
     }
 
     const transporter = nodemailer.createTransport({
-        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 587,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
