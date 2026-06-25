559d586b9e307912fc77a22c4b60b77a47303768
anandayush005 <anandayush233@gmail.com>
2026-06-25 14:04:18 +0530

    Add error logging in resendOtp function for better debugging

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -211,7 +211,7 @@ async function resendOtp(req,res) {
 
     } catch (error) {
 
-        
+        console.log(error)
 
         res.status(500).json({
             message : "Error while generating the otp"
